3303bec13cfc3deb7e032572194fdfbdfd07b2c5
EganJ <eg.co.johnson@gmail.com>
2019-01-12 15:12:57 -0600

    NetworkTables

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -25,6 +25,9 @@ public final class Main {
    * If you change your main robot class, change the parameter type.
    */
   public static void main(String... args) {
+    Network network=new Network();
+    network.testInit();
     RobotBase.startRobot(Robot::new);
+
   }
 }
--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -28,27 +28,26 @@ public class OI {
   // There are a few additional built in buttons you can use. Additionally,
   // by subclassing Button you can create custom triggers and bind those to
   // commands the same as any other Button.
+
+  //// TRIGGERING COMMANDS WITH BUTTONS
+  // Once you have a button, it's trivial to bind it to a button in one of
+  // three ways:
+
+  // Start the command when the button is pressed and let it run the command
+  // until it is finished as determined by it's isFinished method.
+  // button.whenPressed(new ExampleCommand());
+
+  // Run the command while the button is being held down and interrupt it once
+  // the button is released.
+  // button.whileHeld(new ExampleCommand());
+
+  // Start the command when the button is released and let it run the command
+  // until it is finished as determined by it's isFinished method.
+  // button.whenReleased(new ExampleCommand());
   public Joystick primaryJoystick;
   public Button cancelAll;
 
   public OI() {
-
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-
-    // Start the command when the button is released and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
-
     primaryJoystick = new Joystick(0);
     cancelAll = new JoystickButton(primaryJoystick, 7);
     // cancelAll.cancelWhenPressed(Robot.autonomous);
